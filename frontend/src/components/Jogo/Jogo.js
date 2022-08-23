98cda96c72a465606384a0aab1a72fca17c1b780
PedrSV <pedrolucassoaresds@gmail.com>
2022-08-23 09:44:45 -0400

    adicionados alguns movimentos

--- frontend/src/components/Jogo/Jogo.js
+++ frontend/src/components/Jogo/Jogo.js
@@ -1,16 +1,28 @@
 import "./Jogo.css";
+import { useState } from "react";
 import clouds from "../../assets/clouds.png";
 import mario from "../../assets/mario.gif";
-import pipe from "../../assets/pipe.png";
+import cano from "../../assets/pipe.png";
 import gameover from "../../assets/game-over.png";
 
 function Jogo() {
+
+    const [estaPulando, setEstaPulando] = useState(false);
+
+    document.onkeydown = function(){
+        console.log("KeyDown");
+        estaPulando = true;
+    }
+
+    console.log(15, { estaPulando });
+
     return (
         <div className="jogo">
             <img className="nuvens" src={clouds}/>
-            <img className="pipe" src={pipe}/>
+            <img className="cano" src={cano}/>
             <img className="mario" src={mario}/>
             <img className="gameover" src={gameover}/>
+            <div className="chao"></div>
         </div>
     );
 }

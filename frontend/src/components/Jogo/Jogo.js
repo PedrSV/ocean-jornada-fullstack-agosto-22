6e417a9c15ebf0abac80993085197ae3a87f7bea
PedrSV <pedrolucassoaresds@gmail.com>
2022-08-23 11:12:25 -0400

    Estados de Pulo adicionado

--- frontend/src/components/Jogo/Jogo.js
+++ frontend/src/components/Jogo/Jogo.js
@@ -1,5 +1,5 @@
 import "./Jogo.css";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import clouds from "../../assets/clouds.png";
 import mario from "../../assets/mario.gif";
 import cano from "../../assets/pipe.png";
@@ -7,23 +7,65 @@ import gameover from "../../assets/game-over.png";
 
 function Jogo() {
 
-    console.log("Componente de jogo renderizado");
+    //console.log("Componente de jogo renderizado");
 
     const [estaPulando, setEstaPulando] = useState(false);
 
+    // Criamos as referencias para o mario e o cano
+    const marioRef = useRef();
+    const canoRef = useRef();
+
+    function marioEstaNoCano(){
+        const mario = marioRef.current;
+        const cano = canoRef.current;
+
+        if (!mario || !cano){
+            return;
+        }
+
+        return(
+            cano.offsetLeft > mario.offsetLeft &&
+            cano.offsetLeft < mario.offsetLeft + mario.offsetWidth &&
+            mario.offsetTop + mario.offsetHeight > cano.offsetTop
+        );
+    };
+
+    //Implementação temporária para mostrar se o mario esta no cano ou não
+    setInterval(
+        function () {
+            const valor = marioEstaNoCano();
+            console.log("Mário está no cano?", valor);
+        }, 100
+    );
+    
+
     document.onkeydown = function(){
-        console.log("KeyDown");
-        setEstaPulando(truue);
+        //console.log("KeyDown");
+        setEstaPulando(true);
+
+        setTimeout(
+            function(){
+                setEstaPulando(false);
+            },700
+        );
     }
 
-    console.log(15, { estaPulando });
+    // Classe ".mario" padrão do elemento
+    let marioClassName = "mario";
+
+    // Caso estaPulando seja true, adiciona a classe ".mario-pulo" no elemento
+    if(estaPulando){
+        marioClassName = "mario mario-pulo";
+    }
+
+    //console.log(15, { estaPulando });
 
     return (
         <div className="jogo">
             <img className="nuvens" src={clouds}/>
-            <img className="cano" src={cano}/>
-            <img className="mario" src={mario}/>
-            <img className="gameover" src={gameover}/>
+            <img ref={canoRef} className="cano" src={cano}/>
+            <img ref={marioRef} className={marioClassName} src={mario}/>
+            <img hidden className="gameover" src={gameover}/>
             <div className="chao"></div>
         </div>
     );
